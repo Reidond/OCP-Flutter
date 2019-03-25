0506c4b404a8a2b4b43aa6c601372a69e82d61af
Reidond <reidond@gmail.com>
2019-03-25 22:01:40 +0200

    Add home bloc

---
+++ lib/home/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../authentication/index.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final AuthenticationBloc authenticationBloc =
+        BlocProvider.of<AuthenticationBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Open Copyright Platform'),
+      ),
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: Text('Logout'),
+            onPressed: () {
+              authenticationBloc.dispatch(LoggedOut());
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
