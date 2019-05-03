44dc2abcda07a22109aedb677684189a6225aa10
Reidond <reidond@gmail.com>
2019-05-03 20:30:01 +0300

    Add dashboard page

--- lib/common/index.dart
+++ lib/common/index.dart
@@ -1,1 +1,2 @@
 export './loading_indicator.dart';
+export './bottom_loader.dart';
--- lib/dashboard/dashboard_page.dart
+++ lib/dashboard/dashboard_page.dart
@@ -4,68 +4,113 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../authentication/index.dart';
 
+import '../bottom_app_bar/index.dart';
+
+import '../products/index.dart';
+
 class DashBoardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AuthenticationBloc authenticationBloc =
         BlocProvider.of<AuthenticationBloc>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              authenticationBloc.dispatch(LoggedOut());
-            },
-          )
-        ],
-      ),
-      body: Container(),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the Drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Image.asset('assets/icon.png'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+    final BottomAppBarBloc bottomAppBarBloc =
+        BlocProvider.of<BottomAppBarBloc>(context);
+
+    final ProductsBloc productsBloc = BlocProvider.of<ProductsBloc>(context);
+
+    GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+
+    return new Scaffold(
+        key: _scaffoldKey,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Image.asset('assets/icon.png'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
               ),
-            ),
-            ListTile(
-              title: Text('Home'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Products'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Applications'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
+              ListTile(
+                title: Text('Products'),
+                onTap: () {
+                  bottomAppBarBloc.dispatch(BottomAppBarAddProducts());
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Applications'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Profile'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButton: BlocBuilder<BottomAppBarEvent, BottomAppBarState>(
+            bloc: bottomAppBarBloc,
+            builder: (BuildContext context, BottomAppBarState state) {
+              if (state is InitialBottomAppBarState) {
+                return Container(width: 0.0, height: 0.0);
+              }
+              if (state is ProductsPageState) {
+                return FloatingActionButton.extended(
+                  elevation: 4.0,
+                  icon: const Icon(Icons.add),
+                  label: const Text('Add a task'),
+                  onPressed: () {},
+                );
+              }
+            }),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  _scaffoldKey.currentState.openDrawer();
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.exit_to_app),
+                onPressed: () {
+                  authenticationBloc.dispatch(LoggedOut());
+                },
+              )
+            ],
+          ),
+        ),
+        body: BlocProvider(
+          bloc: productsBloc,
+          child: MaterialApp(
+            title: 'Open Copyright Platform',
+            theme: ThemeData.light(),
+            home: BlocBuilder<ProductsEvent, ProductsState>(
+              bloc: productsBloc,
+              builder: (BuildContext context, ProductsState state) {
+                if (state is AuthenticationUnauthenticated) {
+                  //return LoginPage(userRepository: _userRepository);
+                }
+              },
+            ),
+          ),
+        ));
   }
 }
