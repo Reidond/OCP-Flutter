27387f47990fc90747fb9a8ee0af2f79a9463a5c
Reidond <reidond@gmail.com>
2019-05-07 20:50:36 +0300

    Make dashboard second entry point

--- lib/dashboard/dashboard_page.dart
+++ lib/dashboard/dashboard_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_copyright_platform/dashboard/index.dart';
+import 'package:open_copyright_platform/home2/index.dart';
 
 import '../authentication/index.dart';
 
@@ -8,105 +10,60 @@ import '../bottom_app_bar/index.dart';
 
 import '../products/index.dart';
 
-class DashBoardPage extends StatelessWidget {
+class DashBoardPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _DashBoardState();
+  }
+}
+
+class _DashBoardState extends State<DashBoardPage> {
+  BottomAppBarBloc _bottomAppBarBloc;
+  Home2Bloc _home2bloc;
+  DashBoardBloc _dashBoardBloc;
+
+  @override
+  void initState() {
+    _home2bloc = Home2Bloc();
+    _bottomAppBarBloc = BottomAppBarBloc();
+    _dashBoardBloc = DashBoardBloc();
+
+    _bottomAppBarBloc.dispatch(InitialBottomAppBar());
+    _home2bloc.dispatch(InitialHome2());
+    _dashBoardBloc.dispatch(InitialDashBoard());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _home2bloc.dispose();
+    _bottomAppBarBloc.dispose();
+    _dashBoardBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final AuthenticationBloc authenticationBloc =
         BlocProvider.of<AuthenticationBloc>(context);
 
-    final BottomAppBarBloc bottomAppBarBloc =
-        BlocProvider.of<BottomAppBarBloc>(context);
-
-    final ProductsBloc productsBloc = BlocProvider.of<ProductsBloc>(context);
-
     GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
 
     return new Scaffold(
         key: _scaffoldKey,
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Image.asset('assets/icon.png'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text('Products'),
-                onTap: () {
-                  bottomAppBarBloc.dispatch(BottomAppBarAddProducts());
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('Applications'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('Profile'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: BlocBuilder<BottomAppBarEvent, BottomAppBarState>(
-            bloc: bottomAppBarBloc,
-            builder: (BuildContext context, BottomAppBarState state) {
-              if (state is InitialBottomAppBarState) {
-                return Container(width: 0.0, height: 0.0);
-              }
-              if (state is ProductsPageState) {
-                return FloatingActionButton.extended(
-                  elevation: 4.0,
-                  icon: const Icon(Icons.add),
-                  label: const Text('Add a task'),
-                  onPressed: () {},
-                );
-              }
-            }),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: BottomAppBar(
-          child: new Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {
-                  _scaffoldKey.currentState.openDrawer();
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.exit_to_app),
-                onPressed: () {
-                  authenticationBloc.dispatch(LoggedOut());
-                },
-              )
-            ],
-          ),
-        ),
-        body: BlocProvider(
-          bloc: productsBloc,
+        body: BlocProviderTree(
+          blocProviders: [
+            BlocProvider<Home2Bloc>(bloc: _home2bloc),
+            BlocProvider<AuthenticationBloc>(bloc: authenticationBloc),
+            BlocProvider<BottomAppBarBloc>(bloc: _bottomAppBarBloc)
+          ],
           child: MaterialApp(
             title: 'Open Copyright Platform',
-            theme: ThemeData.light(),
-            home: BlocBuilder<ProductsEvent, ProductsState>(
-              bloc: productsBloc,
-              builder: (BuildContext context, ProductsState state) {
-                if (state is AuthenticationUnauthenticated) {
-                  //return LoginPage(userRepository: _userRepository);
+            home: BlocBuilder<DashBoardEvent, DashBoardState>(
+              bloc: _dashBoardBloc,
+              builder: (BuildContext context, DashBoardState state) {
+                if (state is InitialDashBoardState) {
+                  return Home2Page();
                 }
               },
             ),
