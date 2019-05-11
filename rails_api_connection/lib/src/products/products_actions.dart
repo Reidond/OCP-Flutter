f37d1e6de11b283c75dedd702323bcb4ae624533
Reidond <reidond@gmail.com>
2019-05-11 17:56:50 +0300

    Refactor products api
    * Add show call

--- rails_api_connection/lib/src/products/products_actions.dart
+++ rails_api_connection/lib/src/products/products_actions.dart
@@ -1,22 +1,17 @@
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'products.dart';
-import '../app_config.dart';
+import 'package:http/http.dart' as http;
+import 'package:rails_api_connection/src/app_config.dart';
+import 'package:rails_api_connection/src/products/product.dart';
+import 'package:rails_api_connection/src/storage_headers.dart';
 
 class ProductsActions {
   final storage = new FlutterSecureStorage();
 
-  Future<List<Product>> fetchPosts() async {
-    final response =
-        await http.get(AppConfig.API_BASE + 'api/v1/products', headers: {
-      'access-token': await storage.read(key: 'accessToken'),
-      'token-type': await storage.read(key: 'tokenType'),
-      'expiry': await storage.read(key: 'expiry'),
-      'client': await storage.read(key: 'client'),
-      'uid': await storage.read(key: 'uid'),
-    });
+  Future<List<Product>> fetchProducts() async {
+    final response = await http.get('${AppConfig.API_BASE}/api/v1/products',
+        headers: await StorageHeaders.getHeadersFromStorage());
 
     if (response.statusCode == 200) {
       final responseBody = json.decode(response.body);
@@ -32,6 +27,30 @@ class ProductsActions {
               createdAt: rawProduct['created_at'],
               updatedAt: rawProduct['updated_at']);
         }).toList();
+      } else {
+        throw Exception('Success is false');
+      }
+    } else {
+      throw Exception('Error fetching posts');
+    }
+  }
+
+  Future<Product> showProduct(int id) async {
+    final response = await http.get('${AppConfig.API_BASE}/api/v1/products/$id',
+        headers: await StorageHeaders.getHeadersFromStorage());
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      if (responseBody['success'] == true) {
+        final data = json.decode(response.body)['product'];
+        return Product(
+            id: data['id'],
+            userId: data['user_id'],
+            name: data['name'],
+            description: data['description'],
+            productType: data['product_type'],
+            createdAt: data['created_at'],
+            updatedAt: data['updated_at']);
       } else {
         throw Exception('Success is false');
       }
