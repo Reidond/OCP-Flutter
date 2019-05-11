23641c6fb1e0efda0099eeef8fe557832cc4b744
Reidond <reidond@gmail.com>
2019-05-11 17:57:33 +0300

    Add new themes (wip stage)

--- lib/settings/index.dart
+++ lib/settings/index.dart
@@ -1,3 +1,5 @@
 export 'theme_bloc.dart';
 export 'settings_page.dart';
 export 'settings_button.dart';
+export 'theme/theme.dart';
+export 'theme/colors.dart';
--- lib/settings/theme_bloc.dart
+++ lib/settings/theme_bloc.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:open_copyright_platform/settings/theme/theme.dart';
 
 enum ThemeEvent { toggle }
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
   @override
-  ThemeData get initialState => ThemeData.light();
+  ThemeData get initialState => ThemeData.dark();
 
   @override
   Stream<ThemeData> mapEventToState(ThemeEvent event) async* {
