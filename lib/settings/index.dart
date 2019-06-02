c22fbfea207a44b1e6eb544c75fac652c2e0218f
reidond <reidond@gmail.com>
2019-06-02 23:01:03 +0300

    Move settings button

--- lib/settings/index.dart
+++ lib/settings/index.dart
@@ -1,5 +1,13 @@
 export 'theme_bloc.dart';
-export 'components/settings_button.dart';
+
+export 'widgets/settings_button.dart';
+
 export 'pages/settings_page.dart';
-export 'theme/theme.dart';
-export 'theme/colors.dart';
+
+export 'themes/gallery_theme.dart';
+
+export 'package:open_copyright_platform/settings/themes/light/colors.dart';
+export 'package:open_copyright_platform/settings/themes/light/theme.dart';
+
+export 'package:open_copyright_platform/settings/themes/dark/colors.dart';
+export 'package:open_copyright_platform/settings/themes/dark/theme.dart';
