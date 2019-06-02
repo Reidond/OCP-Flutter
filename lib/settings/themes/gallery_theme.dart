8602b74756e90b03cf7c6ecaae9f711a54819daf
reidond <reidond@gmail.com>
2019-06-02 23:01:44 +0300

    Delete not wanted code

---
+++ lib/settings/themes/gallery_theme.dart
@@ -0,0 +1,10 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import 'package:flutter/material.dart';
+
+class GalleryTheme {
+  const GalleryTheme(this.name, this.data);
+  final String name;
+  final ThemeData data;
+}
